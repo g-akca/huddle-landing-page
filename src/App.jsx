9e499a7c3b171f3edc07ec5ff028ceb817e75520
g-akca <gok.akca66@gmail.com>
2026-04-24 21:19:50 +0300

    Create main components, add App styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="text-base leading-base min-h-screen bg-purple-700 bg-[url('/images/bg-mobile.svg')] bg-no-repeat bg-size-[100%_auto] bg-top">
+    <div className="text-base leading-base min-h-screen bg-purple-700 bg-[url('/images/bg-mobile.svg')] bg-no-repeat bg-size-[100%_auto] bg-top flex flex-col items-center gap-13">
+      <div className="px-9 pt-10 flex flex-col gap-16">
+        <Header />
+        <MainSection />
+      </div>
+
+      <Footer />
     </div>
   )
 }
