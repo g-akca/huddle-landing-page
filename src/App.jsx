45591d3517e13a8cfd377087451bda798e31d5ef
g-akca <gok.akca66@gmail.com>
2026-04-24 21:43:35 +0300

    Add MainSection structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="text-base leading-base min-h-screen bg-purple-700 bg-[url('/images/bg-mobile.svg')] bg-no-repeat bg-size-[100%_auto] bg-top flex flex-col gap-13">
+    <div className="text-base leading-base text-white min-h-screen bg-purple-700 bg-[url('/images/bg-mobile.svg')] bg-no-repeat bg-size-[100%_auto] bg-top flex flex-col gap-13">
       <div className="px-9 pt-10 flex flex-col gap-16">
         <Header />
         <MainSection />
