dab7992ae9071c56dd3654e2c7b0a1c49f1c30f3
g-akca <gok.akca66@gmail.com>
2026-04-24 21:54:36 +0300

    Add icons to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+import SocialMediaIcon from './SocialMediaIcon';
+
 function Footer() {
   return (
-    <>
-    </>
+    <div className="self-center flex gap-4">
+      <SocialMediaIcon icon={faFacebookF} />
+      <SocialMediaIcon icon={faTwitter} />
+      <SocialMediaIcon icon={faInstagram} />
+    </div>
   )
 }
 
